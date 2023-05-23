0813a6a3bc19df23e27694e01aeadedf3cfeaf21
unknown <carlos.merinocrespo@gmail.com>
2023-05-23 11:39:30 +0200

    03.BACKEND - Servicios REST de Categorias - GetAllCategories

---
+++ Application/Services/ICategoryService.cs
@@ -0,0 +1,10 @@
+﻿using bootcamp_store_backend.Application.Dtos;
+using bootcamp_store_backend.Domain.Entities;
+
+namespace bootcamp_store_backend.Application.Services
+{
+    public interface ICategoryService
+    {
+        List<CategoryDto> GetAllCategories();
+    }
+}
