9c8655246c2decf78b4273993bf023dfea5f901f
unknown <carlos.merinocrespo@gmail.com>
2023-05-24 12:18:44 +0200

    08.BACKEND - Servicios REST de Items

---
+++ Infrastructure/Persistence/ItemRepository.cs
@@ -0,0 +1,12 @@
+﻿using bootcamp_store_backend.Domain.Entities;
+using bootcamp_store_backend.Domain.Persistence;
+
+namespace bootcamp_store_backend.Infrastructure.Persistence
+{
+    public class ItemRepository : GenericRepository<Item>, IItemRepository
+    {
+        public ItemRepository(StoreContext storeContext) : base(storeContext)
+        {
+        }
+    }
+}
